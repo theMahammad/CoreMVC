99751150af3aa1b73e83c017f2234594547a958b
theMahammad <hssjxie@gmail.com>
2022-06-06 19:57:14 +0400

    wwwroot and client-side library

---
+++ SecondLesson/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecondLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
